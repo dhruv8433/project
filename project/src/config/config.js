bed00cae4577041b25020bd5f1d0268de00a90c7
Dhruv Soni <dhruvrsoni04@gmail.com>
2023-08-09 19:49:12 +0530

    Project

--- project/src/config/config.js
+++
@@ -1,9 +0,0 @@
-//default lang, map api key, firebase config, theme settings default color dark or light,  
-//old map key AIzaSyA0B2eTsnUMMG4SN6Agjz7JD3w_gCDj1lE
-export const defaultLanguage = "en";  // set ISO code here
-export const BASE_URL = "https://edemand-test.thewrteam.in";
-export const API_URL = "https://edemand-test.thewrteam.in/api/v1";
-//old(backup) api key = AIzaSyDArxAFcnCgz7xmveZFLVgWkuocoRfumfA
-export const MAP_API = "AIzaSyA0B2eTsnUMMG4SN6Agjz7JD3w_gCDj1lE";
-export const GOOGLE_MAP = "https://maps.googleapis.com/maps/api/js";
-const geocodeJson = "https://maps.googleapis.com/maps/api/geocode/json";
